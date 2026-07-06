c5cf3038c82c3b882fb5e26f760d4e723911fbcc
Mapleviolet <2217998169@qq.com>
2026-07-06 11:46:03 +0800

    AC

---
+++ 温度转换（Easy）.cpp
@@ -0,0 +1,27 @@
+﻿//#include <vector>
+//
+//class Solution 
+//{
+//public:
+//    std::vector<double> convert_temperature(double celsius)
+//    {
+//        std::vector<double> ans;
+//        ans.push_back(celsius + 273.15);
+//        ans.push_back(1.80 * celsius + 32.00);
+//        return ans;
+//    }
+//};
+//
+////你过关
+// 
+////这边还有更简洁的做法
+//#include <vector>
+//
+//class Solution 
+//{
+//public:
+//    std::vector<double> convert_temperature(double celsius)
+//    {
+//        return { celsius + 273.15, celsius * 1.80 + 32.00 };
+//    }
+//};
