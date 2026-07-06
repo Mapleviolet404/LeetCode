34d487fddd75b7675f30f9be6b5eba25826e456c
Mapleviolet <2217998169@qq.com>
2026-07-06 11:49:27 +0800

    AC

---
+++ 两整数相加（Easy）.py
@@ -0,0 +1,10 @@
+# class Solution(object):
+#     def sum(self, num1: int, num2: int) -> int:
+#         """
+#         :type num1: int
+#         :type num2: int
+#         :rtype: int
+#         """
+#         return num1 + num2
+#
+# # 你过关
---
+++ 温度转换（Easy）.py
@@ -0,0 +1,4 @@
+# class Solution:
+#     def convert_temperature(self, celsius: float) -> list[float]:
+#         return[celsius + 273.15 , celsius * 1.80 + 32.00]
+# # 你过关
